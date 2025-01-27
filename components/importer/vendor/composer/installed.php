355ea47dafe090306dd4bb3451b9241d08ea239a
Alexus450 <Samarschi@gmail.com>
2025-01-27 14:41:40 +0200

    add vendor directory to git

---
+++ components/importer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'inspiro-starter-sites/importer',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'awesomemotive/wp-content-importer-v2' => array(
+            'pretty_version' => 'v3.0.5',
+            'version' => '3.0.5.0',
+            'reference' => 'd0628afdbe2bcdde408e66b284e4cc30d7cc711d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../awesomemotive/wp-content-importer-v2',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'inspiro-starter-sites/importer' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
